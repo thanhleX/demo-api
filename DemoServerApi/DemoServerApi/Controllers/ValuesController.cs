88fa3903c5f14e4def5f1a3815c46dbca71bd877
thanhleX <145445640+thanhleX@users.noreply.github.com>
2024-01-26 00:16:48 +0700

    Add files via upload

---
+++ DemoServerApi/DemoServerApi/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoServerApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Message = "Hello from API!" });
+        }
+    }
+
+}
