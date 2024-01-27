1431f1f012ffc613ce698362c66131c1e21d33e4
thanhleX <145445640+thanhleX@users.noreply.github.com>
2024-01-27 14:11:01 +0700

    Delete DemoApi directory

--- DemoApi/DemoServerApi/Models/Booking.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DemoServerApi.Models
-{
-    public class Booking
-    {
-        public int BookingId { get; set; }
-        public int UserId { get; set; }
-        public DateTime BookingDate { get; set; }
-    }
-}
